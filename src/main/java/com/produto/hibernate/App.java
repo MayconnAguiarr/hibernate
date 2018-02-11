acc90c8761e4d1cb2ad0d079f93b66c38462d298
Maycon <mayconn.aguiarr@gmail.com>
2018-02-11 12:16:08 -0200

    realizado inserção no banco

--- src/main/java/com/produto/hibernate/App.java
+++ src/main/java/com/produto/hibernate/App.java
@@ -1,13 +1,37 @@
 package com.produto.hibernate;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.produto.hibernate.model.Reminder;
+
+public class App {
+
+	private static EntityManagerFactory entityManagerFactory;
+
+	public static void main(String[] args) {
+		entityManagerFactory = Persistence.createEntityManagerFactory("hibernatejpa");
+		insert();
+	}
+
+	public static void insert() {
+
+		Reminder reminder = new Reminder();
+		reminder.setTitle("Comprar leite");
+		reminder.setDescription("Hoje, 10:30");
+
+		EntityManager em = entityManagerFactory.createEntityManager();
+
+		try {
+			em.getTransaction().begin();
+			em.persist(reminder);
+			em.getTransaction().commit();
+		} catch (Exception e) {
+			em.getTransaction().rollback();
+			System.out.println("INSERT: " + e.getMessage());
+		} finally {
+			em.close();
+		}
+	}
 }
