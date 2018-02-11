c8a56913536ee266b1424ae6d5fe0bb12f4f1660
Maycon <mayconn.aguiarr@gmail.com>
2018-02-11 10:35:52 -0200

    adicionado mapeamento jpa na classe Reminder

--- src/main/java/com/produto/hibernate/model/Reminder.java
+++ src/main/java/com/produto/hibernate/model/Reminder.java
@@ -1,9 +1,22 @@
 package com.produto.hibernate.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Reminder {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+	
+	@Column(nullable = false)
 	private String title;
+	
+	@Column(nullable = false)
 	private String description;
 
 	public Reminder() {	}
